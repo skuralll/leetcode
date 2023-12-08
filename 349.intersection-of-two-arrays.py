2da554d7b226f2711d6f1c04e6dcbcc5e7c7d1f6
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-12-08 16:20:59 +0900

    解答:349

---
+++ 349.intersection-of-two-arrays.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=349 lang=python3
+#
+# [349] Intersection of Two Arrays
+#
+
+
+# @lc code=start
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1, set2 = set(nums1), set(nums2)
+        return list(set1 & set2)
+
+
+# @lc code=end
