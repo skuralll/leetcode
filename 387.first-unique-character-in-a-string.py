ce1363743b3efc03e9cf37d2b6fb3f6d1d7a6e48
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-12-08 16:58:32 +0900

    改善:387

--- 387.first-unique-character-in-a-string.py
+++ 387.first-unique-character-in-a-string.py
@@ -8,16 +8,15 @@
 # @lc code=start
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        dic = {}
-        sl = list(s)
-        for i in range(len(sl)):
-            if sl[i] in dic.keys():
-                dic[sl[i]][1] += 1
+        count = {}
+        for c in list(s):
+            if c in count.keys():
+                count[c] += 1
             else:
-                dic[sl[i]] = [i, 1]
-        for key in dic.keys():
-            if dic[key][1] == 1:
-                return dic[key][0]
+                count[c] = 1
+        for i in range(len(s)):
+            if count[s[i]] == 1:
+                return i
         return -1
 
 
