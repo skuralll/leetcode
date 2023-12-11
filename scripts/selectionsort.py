9425eb3a16a5c45f03b3563539ccaff21d6ea607
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-12-11 19:15:06 +0900

    学習:選択ソート

---
+++ scripts/selectionsort.py
@@ -0,0 +1,18 @@
+from random import shuffle
+
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+array = list(range(30))
+shuffle(array)
+
+print(selection_sort(array))
